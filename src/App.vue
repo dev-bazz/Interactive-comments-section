cc9db35dd574d45c02eb4456ee36cf7bb72e89cc
bazz <clement.f.bazz@gmail.com>
2023-03-04 11:33:49 +0100

    setup-files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+	import Comment from "./components/Comment.vue";
 	import { ref } from "vue"; // Vue3 Dependencies
+	const hello = ref("hello World");
 </script>
 
 <template>
 	<h1>Hello World</h1>
+	<Comment msg="By BaZz" />
 </template>
---
+++ src/components/Comment.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+	defineProps({ msg: String });
+</script>
+
+<template>
+	<h1>Comment {{ msg }}</h1>
+</template>
